bfdab1dcf09ff5a46e290f4ddc10c1a00971443d
Myxmax <102454137+Myxmax@users.noreply.github.com>
2026-07-16 08:08:18 +0330

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-base: '/restaurant-menu/',
+
 export default defineConfig(() => {
   return {
+    base: '/restaurant-menu/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
